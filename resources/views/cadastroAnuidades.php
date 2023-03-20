1c07eeebec91516f3033a9643fe6f2dd6261224a
leo <l30cavalcanti@protonmail.com>
2023-03-20 15:19:03 -0300

    adicionado views de cadastro de anuidades e cadastro de associado

---
+++ resources/views/cadastroAnuidades.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <h1>Cadastro de anuidades</h1>
+        <form action="/action_page.php" method="post">
+            <label for="lname">Ano:</label>
+            <input type="text" id="ano" name="ano"><br><br>
+            <label for="valor">Valor da anuidade:</label>
+            <input type="text" id="valor" name="valor"><br><br>
+            <input type="submit" value="Efetuar cadastro da anuidade">
+        </form> 
+    </head>
+</html>
---
+++ resources/views/cadastroAssociado.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <h1>Cadastro</h1>
+        <form action="/action_page.php" method="post">
+            <label for="fname">Primeiro nome:</label>
+            <input type="text" id="fname" name="fname"><br><br>
+            <label for="lname">Sobrenomes:</label>
+            <input type="text" id="lname" name="lname"><br><br>
+            <label for="data">Data de filiação:</label>
+            <input type="date" id="data" name="data"><br><br>
+            <label for="cpf">CPF:</label>
+            <input type="text" id="cpf" name="cpf"><br><br>
+            <input type="submit" value="Efetuar cadastro">
+        </form> 
+    </head>
+</html>
